2733f854f555bceded3441fac05675d92fc7839b
ritik <honeyritikjob75@gmail.com>
2025-11-15 13:41:30 +0530

    final commit

--- src/JavaForPractice/BubbleSort.java
+++ src/JavaForPractice/BubbleSort.java
@@ -36,7 +36,7 @@ public class BubbleSort {
 		System.out.println("Hello World2" );
 		System.out.println("Hello World3" );
 		System.out.println("Hello World4" );
-		System.out.println("Hello World5" );
+		System.out.println("Hello World7" );
 	}
 	
 	
